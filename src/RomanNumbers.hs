3d19663ebb302f5e0149f7feaeb14293b1e56c6d
20192656 <r.g.m.v.veldhoven@student.tue.nl>
2021-09-16 17:19:07 +0200

    Finished TO DO

--- src/RomanNumbers.hs
+++ src/RomanNumbers.hs
@@ -1,8 +1,8 @@
 {-|
 Module      : RomanNumbers
 Description : Convert Roman numbers to integers and the other way around.
-Copyright   : STUDENT NAME 1 (ID)
-              STUDENT NAME 2 (ID)
+Copyright   : Raymen van Veldhoven (1439499)
+              Dalton Harmsen (ID)
 
 RomanNumbers is a module to convert Roman numbers to integers and vice versa. Roman numbers are represented by the characters 'M', 'D', 'C', 'L', 'X', 'V', and 'I'
 
@@ -17,7 +17,27 @@ module RomanNumbers
 
 
 -- Implement and document r2i
-r2i rs = 1
+r2i :: [Char] -> Int
+r2i [] = 0
+r2i (r:rs) | r == 'I' = 1 + r2i rs
+           | r == 'V' = 5 + r2i rs
+           | r == 'X' = 10 + r2i rs
+           | r == 'L' = 50 + r2i rs
+           | r == 'C' = 100 + r2i rs
+           | r == 'D' = 500 + r2i rs
+           | r == 'M' = 1000 + r2i rs
+           | otherwise = error ("Unexpected character;" ++ [r] ++ " is not a Roman numeral.")
+
 
 -- Implement and document i2r
-i2r n  = "I"
+i2r :: Int -> [Char]
+i2r 0 = ""
+i2r a | a >= 1000 = 'M' : i2r (a-1000)
+      | a >= 500 = 'D' : i2r (a-500)
+      | a >= 100 = 'C' : i2r (a-100)
+      | a >= 50 = 'L' : i2r (a-50)
+      | a >= 10 = 'X' : i2r (a-10)
+      | a >= 5 = 'V' : i2r (a-5)
+      | a >= 1 = 'I' : i2r (a-1)
+      | otherwise = error "There are no negative Roman numbers"
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,7 @@
 import Test.Hspec
 import Test.QuickCheck
 import RomanNumbers (r2i, i2r)
+import Control.Exception (evaluate)
 
 -- A QuickCheck generator for natural number between 1 and 10000.
 naturals :: Gen Int
@@ -14,14 +15,45 @@ main = hspec $ do
       it "should convert \"I\" to 1" $ do
         r2i "I" `shouldBe` (1::Int)
 
-      -- Here go your own tests for r2i.
+      it "should convert \"X\" to 10" $ do
+        r2i "X" `shouldBe` (10::Int)
+
+      it "should convert \"M\" to 1000" $ do
+        r2i "M" `shouldBe` (1000::Int)
+
+      it "should convert \"XIIII\" to 14" $ do
+        r2i "XIIII" `shouldBe` (14::Int)
+      
+      it "should convert \"VI\" to 6" $ do
+        r2i "VI" `shouldBe` (6::Int)
+
+      it "should convert \"\" to 0" $ do
+        r2i "" `shouldBe` (0::Int)
+
+      it "should convert \"P\" to error" $ do
+        evaluate (r2i "P") `shouldThrow` anyErrorCall
   
     describe "i2r" $ do
 
       it "should convert 1 to \"I\"" $ do
         i2r (1::Int) `shouldBe` "I"
 
-      -- Here go your own tests for i2r.
+      it "should convert 10 to \"X\"" $ do
+        i2r (10::Int) `shouldBe` "X"
+      
+      it "should convert 15 to \"XV\"" $ do
+        i2r (15::Int) `shouldBe` "XV"
+
+      it "should convert 1000 to \"M\"" $ do
+        i2r (1000::Int) `shouldBe` "M"
+
+      it "should convert 0 to \"\"" $ do
+        i2r (0::Int) `shouldBe` ""
+
+      it "should convert -10 to error" $ do
+        evaluate (i2r ((-10)::Int)) `shouldThrow` anyErrorCall
+
+
 
 
 -- Do not change anything below this line
@@ -30,5 +62,8 @@ main = hspec $ do
 
       it "(forall n : n in N : (r2i . i2r) n >= 1)" $ property $
         forAll naturals (\n -> (r2i . i2r) n >= (1::Int))
+
+      it "(forall n : n in N : (r2i . i2r) n >= n)" $ property $
+        forAll naturals (\n -> (r2i . i2r) n == n)
 
       -- Here go your own tests for r2i . i2r.
